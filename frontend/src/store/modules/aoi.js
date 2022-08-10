fd095882df066f04a37516c5b58ce05bb59f1dde
QSafariallahkheili <safari.qassem@gmail.com>
2022-08-10 16:09:30 +0200

    UCD-38-show-buildings: save and retrieve buildings

--- frontend/src/store/modules/aoi.js
+++ frontend/src/store/modules/aoi.js
@@ -1,13 +1,51 @@
+import { HTTP } from '../../utils/http-common';
+import {GeoJsonLayer} from '@deck.gl/layers';
+import {MapboxLayer} from '@deck.gl/mapbox';
+
 const aoi = {
     namespaced: true,
     state: {
-        bbox : {xmin: 13.742725, ymin: 51.059803, xmax: 13.756758, ymax: 51.066950}
+        bbox : {xmin: 13.742725, ymin: 51.059803, xmax: 13.756758, ymax: 51.066950},
+        overpassBuildings: null,
 
     },
     mutations:{
 
     },
     actions:{
+        getbuildingsFromOSM({state}){
+            HTTP
+            .post('get-buildings-from-osm', {
+                bbox : state.bbox
+            })
+        },
+        getbuildingsFromDB({state, rootState}){
+            HTTP
+            .get('get-buildings-from-db')
+            .then(response=>{
+                console.log(response)
+                state.overpassBuildings = new MapboxLayer({
+                    id: 'overpass_buildings',
+                    type: GeoJsonLayer,
+                    data: response.data.features,
+                    opacity: 1,
+                    stroked: false,
+                    filled: true,
+                    extruded: true,
+                    wireframe: false,
+                    getElevation: f => f.properties.estimatedheight,
+                    getFillColor: [235, 148, 35, 255],
+                    getLineColor: [0, 0, 0],
+                    wireframe: true,
+                    pickable: true,
+                    
+                });
+                rootState.map.map.addLayer(state.overpassBuildings);
+                rootState.map.map.on('click', 'overpass_buildings', (e) => {
+                    console.log(e)
+                })
+            })
+        },
 
     },
     getters:{
