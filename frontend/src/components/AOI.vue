959553111acc5cc596a707ef8f66ca6d6282b7de
QSafariallahkheili <safari.qassem@gmail.com>
2022-08-10 13:43:52 +0200

    add UI for creating AOI

---
+++ frontend/src/components/AOI.vue
@@ -0,0 +1,29 @@
+<template>
+<v-col
+    cols="10"
+    sm="1"
+    style= "position: absolute; right: 0; top: 60px;z-index:999"
+>
+    <v-select
+        :items="['get', 'retrieve']"
+        label="building"
+        variant="outlined"
+        @update:modelValue="sendBuildingRequest"
+    ></v-select>
+   
+   
+</v-col>
+
+
+</template>
+
+<script setup>
+
+const sendBuildingRequest = (e)=>{
+    console.log(e)
+}
+</script>
+
+<style scoped>
+
+</style>
