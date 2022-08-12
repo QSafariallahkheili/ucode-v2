ca1eda5a8f35762f6f613caa5d064c9ed574cb88
Dobroslav Totev <dev@totev.de>
2022-08-12 10:38:17 +0200

    fix(backend): fix cors origin

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,8 @@ from db import get_table_names, get_buildings_from_osm, init_building_table, get
 
 app = FastAPI()
 origins = [
-    "https://*.urban-codesign.com",
+    "https://api.v2.urban-codesign.com",
+    "https://v2.urban-codesign.com",
     "http://localhost",
     "http://localhost:8080"
 ]
