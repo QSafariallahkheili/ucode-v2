e5f9f03072c02911bd4a4239cfac81b363a158bd
QSafariallahkheili <safari.qassem@gmail.com>
2023-03-09 14:36:42 +0100

    test the effect of ANALYZE on table indexing

--- backend/db_migrations/0017.create-more-spatial-indexes.sql
+++ backend/db_migrations/0017.create-more-spatial-indexes.sql
@@ -2,3 +2,7 @@ CREATE INDEX IF NOT EXISTS idx_bike_geom ON bike USING gist (geom);
 CREATE INDEX IF NOT EXISTS idx_driving_lane_polygon_geom ON driving_lane_polygon USING gist (geom);
 CREATE INDEX IF NOT EXISTS idx_sidewalk_geom ON sidewalk USING gist (geom);
 CREATE INDEX IF NOT EXISTS idx_traffic_signal_geom ON traffic_signal USING gist (geom);
+ANALYZE bike;
+ANALYZE driving_lane_polygon;
+ANALYZE sidewalk;
+ANALYZE traffic_signal;
